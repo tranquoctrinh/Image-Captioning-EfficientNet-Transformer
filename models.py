64f4d97449122805bfabbb46e394e4783155f8f6
tranquoctrinh <tranquoctrinh.ai@gmail.com>
2022-03-30 17:59:15 +0000

    change attention

--- models.py
+++ models.py
@@ -5,6 +5,41 @@ import torchvision
 import torchvision.transforms as transforms
 # import efficientnet model
 from efficientnet_pytorch import EfficientNet
+import math
+from torch.autograd import Variable
+
+# Embedding the input sequence
+class Embedding(nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(Embedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+    def forward(self, x):
+        return self.embedding(x)
+
+# The positional encoding vector
+class PositionalEncoder(nn.Module):
+    def __init__(self, embedding_dim, max_seq_length=512, dropout=0.1):
+        super(PositionalEncoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.dropout = nn.Dropout(dropout)
+        pe = torch.zeros(max_seq_length, embedding_dim)
+        for pos in range(max_seq_length):
+            for i in range(0, embedding_dim, 2):
+                pe[pos, i] = math.sin(pos/(10000**(2*i/embedding_dim)))
+                pe[pos, i+1] = math.cos(pos/(10000**((2*i+1)/embedding_dim)))
+        pe = pe.unsqueeze(0)        
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+        x = x*math.sqrt(self.embedding_dim)
+        seq_length = x.size(1)
+        pe = Variable(self.pe[:, :seq_length], requires_grad=False).to(x.device)
+        # Add the positional encoding vector to the embedding vector
+        x = x + pe
+        x = self.dropout(x)
+        return x
+
 
 # Encoder model image captioning with EfficientNet
 class Encoder(nn.Module):
@@ -19,41 +54,7 @@ class Encoder(nn.Module):
         features = features.permute(0, 2, 3, 1)
         # features: (batch_size, 7, 7, 2048)
         return features
-
-# SoftAttention
-class SoftAttention(nn.Module):
-    """
-    Attention Network.
-    """
-
-    def __init__(self, encoder_dim, decoder_dim, attention_dim):
-        """
-        :param encoder_dim: feature size of encoded images
-        :param decoder_dim: size of decoder's RNN
-        :param attention_dim: size of the attention network
-        """
-        super(Attention, self).__init__()
-        self.encoder_att = nn.Linear(encoder_dim, attention_dim)
-        self.decoder_att = nn.Linear(decoder_dim, attention_dim)
-        self.full_att = nn.Linear(attention_dim, 1)
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, encoder_out, decoder_hidden):
-        """
-        Forward propagation.
-        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
-        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
-        :return: attention weighted encoding, weights
-        """
-        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
-        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
-        alpha = self.softmax(att)  # (batch_size, num_pixels)
-        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
-
-        return attention_weighted_encoding, alpha
-        
+      
 
 # Self-attention layer
 class SelfAttention(nn.Module):
@@ -109,9 +110,32 @@ class MultiHeadAttention(nn.Module):
         output = self.out(output)
         return output
 
+# SoftAttention
+class SoftAttention(nn.Module):
+    """
+    Attention Network.
+    """
+
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        super(Attention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)
+        self.full_att = nn.Linear(attention_dim, 1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, memory, decoder_hidden):
+        att1 = self.encoder_att(memory)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+        return attention_weighted_encoding, alpha
+
 # Transformer decoder layer
 class DecoderLayer(nn.Module):
-    def __init__(self, embedding_dim, num_heads, hidden_dim, dropout=0.1):
+    def __init__(self, embedding_dim=512, num_heads=8, dropout=0.1):
         super(DecoderLayer, self).__init__()
         self.self_attention = MultiHeadAttention(embedding_dim, num_heads, dropout)
         self.encoder_attention = SoftAttention(embedding_dim, hidden_dim, attention_dim)
@@ -127,7 +151,7 @@ class DecoderLayer(nn.Module):
         self.norm2 = Norm(embedding_dim)
         self.norm3 = Norm(embedding_dim)
 
-    def forward(self, x, memory, source_mask, target_mask):
+    def forward(self, x, memory, target_mask):
         x2 = self.norm1(x)
         x = x + self.dropout1(self.self_attention(x2, x2, x2, target_mask))
         x2 = self.norm2(x)
@@ -136,12 +160,12 @@ class DecoderLayer(nn.Module):
         x = x + self.dropout3(self.feed_forward(x2))
         return x
 
+
 # Decoder model image captioning with Attention
 class Decoder(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, attention_dim, vocab_size, num_layers, dropout=0.1):
         super(Decoder, self).__init__()
         self.embed = nn.Embedding(vocab_size, embedding_dim)
-        self.attention = SoftAttention(embedding_dim, hidden_dim, attention_dim)
         self.layers = nn.ModuleList([DecoderLayer(embedding_dim, num_heads, hidden_dim, dropout) for _ in range(num_layers)])
         self.fc = nn.Linear(hidden_dim, vocab_size)
         self.dropout = nn.Dropout(p=0.5)
