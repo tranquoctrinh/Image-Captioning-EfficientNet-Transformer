cab18b0e9d0517eb19a07c7503a46c670af87195
tranquoctrinh <tranquoctrinh.ai@gmail.com>
2022-03-30 17:33:49 +0000

    create model

--- models.py
+++ models.py
@@ -48,7 +48,6 @@ class SoftAttention(nn.Module):
         # (batch_size, max_len, 7, 7)
         return context
         
-
 # Self-attention layer
 class SelfAttention(nn.Module):
     ''' Scaled Dot-Product Attention '''
