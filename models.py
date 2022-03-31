ee04cb9943b6d405906ed0d975c9622e05f54b7b
tranquoctrinh <tranquoctrinh.ai@gmail.com>
2022-03-31 07:46:36 +0000

    debug models

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ import torchvision.transforms as transforms
 from efficientnet_pytorch import EfficientNet
 import math
 from torch.autograd import Variable
+import numpy as np
 
 # Embedding the input sequence
 class Embedding(nn.Module):
@@ -16,6 +17,15 @@ class Embedding(nn.Module):
 
     def forward(self, x):
         return self.embedding(x)
+
+# Norm layer
+class Norm(nn.Module):
+    def __init__(self, embedding_dim):
+        super(Norm, self).__init__()
+        self.norm = nn.LayerNorm(embedding_dim)
+
+    def forward(self, x):
+        return self.norm(x)
 
 # The positional encoding vector
 class PositionalEncoder(nn.Module):
@@ -117,7 +127,8 @@ class SoftAttention(nn.Module):
     """
 
     def __init__(self, encoder_dim, decoder_dim, attention_dim):
-        super(Attention, self).__init__()
+        super(SoftAttention, self).__init__()
+        self.encoder_dim = encoder_dim
         self.encoder_att = nn.Linear(encoder_dim, attention_dim)
         self.decoder_att = nn.Linear(decoder_dim, attention_dim)
         self.full_att = nn.Linear(attention_dim, 1)
@@ -125,92 +136,98 @@ class SoftAttention(nn.Module):
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, memory, decoder_hidden):
+        batch_size = memory.size(0) # batch size
+        memory = memory.view(batch_size, -1, self.encoder_dim)  # (batch_size, num_pixels, encoder_dim)
         att1 = self.encoder_att(memory)  # (batch_size, num_pixels, attention_dim)
-        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
-        alpha = self.softmax(att)  # (batch_size, num_pixels)
-        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
-
-        return attention_weighted_encoding, alpha
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, max_seq_len, attention_dim)
+        att = self.softmax(torch.matmul(att1, att2.transpose(1, 2)))/np.sqrt(self.encoder_dim)
+        # (batch_size, num_pixels, max_seq_len)
+        return att
 
 # Transformer decoder layer
 class DecoderLayer(nn.Module):
-    def __init__(self, embedding_dim=512, num_heads=8, dropout=0.1):
+    def __init__(self, embedding_dim, attention_dim, num_heads, dropout=0.1):
         super(DecoderLayer, self).__init__()
         self.self_attention = MultiHeadAttention(embedding_dim, num_heads, dropout)
-        self.encoder_attention = SoftAttention(embedding_dim, hidden_dim, attention_dim)
+        self.encoder_attention = SoftAttention(embedding_dim, embedding_dim, attention_dim)
         self.feed_forward = nn.Sequential(
-            nn.Linear(embedding_dim, embedding_dim * 4),
+            nn.Linear(embedding_dim + 49, embedding_dim * 2),
             nn.ReLU(),
-            nn.Linear(embedding_dim * 4, embedding_dim)
+            nn.Linear(embedding_dim * 2, embedding_dim)
         )
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
         self.dropout3 = nn.Dropout(dropout)
         self.norm1 = Norm(embedding_dim)
         self.norm2 = Norm(embedding_dim)
-        self.norm3 = Norm(embedding_dim)
+        self.norm3 = Norm(embedding_dim + 49)
 
     def forward(self, x, memory, target_mask):
         x2 = self.norm1(x)
         x = x + self.dropout1(self.self_attention(x2, x2, x2, target_mask))
-        x2 = self.norm2(x)
-        x = x + self.dropout2(self.encoder_attention(x2, memory, memory, source_mask))
-        x2 = self.norm3(x)
+        x2 = self.norm1(x)
+        att = self.dropout2(self.encoder_attention(memory, x2)) # (batch_size, num_pixels, max_seq_len)
+        att = att.transpose(1, 2) # (batch_size, max_seq_len, num_pixels)
+        x2 = torch.cat([x, att], dim=-1) # (batch_size, max_seq_len, embedding_dim + num_pixels)
+        x2 = self.norm3(x2)
         x = x + self.dropout3(self.feed_forward(x2))
         return x
 
 
 # Decoder model image captioning with Attention
 class Decoder(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, attention_dim, vocab_size, num_layers, dropout=0.1):
+    def __init__(self, embedding_dim, attention_dim, vocab_size, max_seq_len, num_layers, num_heads, dropout=0.1):
         super(Decoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.vocab_size = vocab_size
         self.embed = nn.Embedding(vocab_size, embedding_dim)
-        self.layers = nn.ModuleList([DecoderLayer(embedding_dim, num_heads, hidden_dim, dropout) for _ in range(num_layers)])
-        self.fc = nn.Linear(hidden_dim, vocab_size)
-        self.dropout = nn.Dropout(p=0.5)
+        self.layers = nn.ModuleList([DecoderLayer(embedding_dim, attention_dim, num_heads, dropout) for _ in range(num_layers)])
+        self.dropout = nn.Dropout(dropout)
         self.norm = Norm(embedding_dim)
+        self.position_embedding = PositionalEncoder(embedding_dim, max_seq_len, dropout)
 
-    def forward(self, target, memory, source_mask, target_mask):
+    def forward(self, target, memory, target_mask):
         # target: (batch_size, seq_len)
         # memory is the encoder output
         x = self.embed(target)
-        x = self.norm(x)
+        # Add the position embeddings
+        x = self.position_embedding(x)
         for layer in self.layers:
-            x = layer(x, memory, source_mask, target_mask)
-        x = self.dropout(x)
-        x = self.fc(x)
+            x = layer(x, memory, target_mask)
+        x = self.norm(x)
         return x
 
 # Model image captioning with Attention
 class ImageCaptioning(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, attention_dim, vocab_size, num_layers, num_heads, dropout=0.1):
+    def __init__(self, embedding_dim=512, attention_dim=256, vocab_size=0, max_seq_len=256, num_layers=8, num_heads=8, dropout=0.1):
         super(ImageCaptioning, self).__init__()
-        self.encoder = Encoder(embedding_dim)
-        self.decoder = Decoder(embedding_dim, hidden_dim, attention_dim, vocab_size, num_layers, dropout)
+        self.position_embedding = PositionalEncoder(embedding_dim, max_seq_len, dropout)
+        self.encoder = Encoder()
+        self.decoder = Decoder(embedding_dim, attention_dim, vocab_size, max_seq_len, num_layers, num_heads, dropout)
         self.embed = nn.Embedding(vocab_size, embedding_dim)
         self.norm = Norm(embedding_dim)
-        self.fc = nn.Linear(hidden_dim, vocab_size)
+        self.fc = nn.Linear(embedding_dim, vocab_size)
         self.dropout = nn.Dropout(p=0.5)
 
-    def forward(self, image, captions, lengths):
+    def forward(self, image, captions):
         # image: (batch_size, embedding_dim)
         # captions: (batch_size, max_len)
         # lengths: (batch_size)
         batch_size = image.size(0)
         # Encode the image
         encoder_output = self.encoder(image)
-        # Create the mask
-        target_mask = torch.zeros(batch_size, captions.size(1), dtype=torch.float)
-        for i, length in enumerate(lengths):
-            target_mask[i, length:] = 1
+        # Create captions mask
+        target_mask = self.make_mask(captions)
         # Decode the image
-        decoder_output = self.decoder(captions, encoder_output, target_mask, target_mask)
+        decoder_output = self.decoder(captions, encoder_output, target_mask)
         # Apply the linear projection
         decoder_output = self.fc(decoder_output)
         return decoder_output
-
-
+    
+    def make_mask(self, target_ids):
+        batch_size, len_target = target_ids.size()
+        subsequent_mask = (1 - torch.triu(torch.ones((1, len_target, len_target), device=target_ids.device), diagonal=1)).bool()
+        return subsequent_mask
 
 def main():
     # transform
@@ -220,3 +237,13 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+    batch_size = 4
+    max_seq_len = 128
+    vocab_size = 123456
+    model = ImageCaptioning(embedding_dim=2048, attention_dim=256, vocab_size=vocab_size, num_layers=8, num_heads=8, dropout=0.1)
+    out = model(torch.randn(batch_size, 3, 224, 224), torch.randint(0, vocab_size, (batch_size, max_seq_len)))
+
+    import ipdb; ipdb.set_trace()
+if __name__ == "__main__":
+    main()
