63782407ae940a9a53004985695365003a672aad
tranquoctrinh <tranquoctrinh.ai@gmail.com>
2022-04-04 04:17:51 +0000

    add preprocessing image, faster training

--- datasets.py
+++ datasets.py
@@ -9,6 +9,22 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 from transformers import BertTokenizer
 import json
+from tqdm import tqdm
+
+
+
+def preprocess_image(configs, transform):
+    """
+    This function preprocesses the image and saves it in the image_dir.
+    I's faster for the training process to load the images from the torch file.
+    """
+    karpathy = json.load(open(configs["karpathy_json_path"], "r"))
+    bar = tqdm(karpathy["images"])
+    for image in bar:
+        image_path = os.path.join(configs["image_dir"], image["file_path"])
+        image = Image.open(image_path).convert("RGB")
+        image = transform(image)
+        torch.save(image, image_path.replace(".jpg", ".pt"))
 
 
 class ImageCaptionDataset(Dataset):
@@ -47,9 +63,14 @@ class ImageCaptionDataset(Dataset):
     
     def __getitem__(self, index):
         image_path = self.df.iloc[index]["image_path"]
-        image = Image.open(image_path).convert("RGB")
-        if self.transform is not None:
-            image = self.transform(image)
+        image_torch_path = image_path.replace(".jpg", ".pt")
+        if os.path.exists(image_torch_path):
+            image = torch.load(image_torch_path)
+        else:
+            image = Image.open(image_path).convert("RGB")
+            if self.transform is not None:
+                image = self.transform(image)
+            torch.save(image, image_torch_path)
 
         caption = self.df.loc[index, "caption"]
         caption_tokens = self.tokenizer(caption, max_length=self.max_seq_len, padding="max_length", truncation=True, return_tensors="pt")["input_ids"][0]
--- models.py
+++ models.py
@@ -145,6 +145,25 @@ class SoftAttention(nn.Module):
         
         return att
 
+# Bahdanau Attention
+class BahdanauAttention(nn.Module):
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        super(BahdanauAttention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+    def forward(self, encoder_out, decoder_hidden):
+        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+        return attention_weighted_encoding, alpha
+
+
 # Transformer decoder layer
 class DecoderLayer(nn.Module):
     def __init__(self, embedding_dim, attention_dim, num_heads, dropout=0.1):
